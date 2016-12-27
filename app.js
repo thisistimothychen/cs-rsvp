95645a4a4f8989cf96a27d8d2477d794d4710278
Allen Cheng <computer_allen@yahoo.com>
2016-12-26 21:16:43 -0500

    (Very) crude CAS login integration skeleton

--- app.js
+++ app.js
@@ -45,6 +45,28 @@ const passportConfig = require('./config/passport');
  */
 const app = express();
 
+var CAS = require('cas');
+var cas = new CAS({
+    base_url: 'https://login.umd.edu/cas',
+    service: 'my_service',
+    version: 3.0
+});
+
+var cas_login = function(req, res) {
+    cas.authenticate(req, res, function(err, status, username, extended) {
+        if (err) {
+            // Handle the error
+            res.send({error: err});
+        } else {
+            // Log the user in
+            res.send({status: status, username: username, attributes: extended.attributes});
+        }
+    });
+}
+
+app.get('/ggwp', cas_login);
+
+
 /**
  * Connect to MongoDB.
  */
