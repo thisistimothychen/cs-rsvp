7b2bfadcf3ccb264b4abe7a7c40dcdc83d458f19
Allen Cheng <allen12@umd.edu>
2017-02-10 01:02:38 -0500

    Make uploads folder serve static content

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 // Moment.js
 let moment = require('moment');
